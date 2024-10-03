ff5b00ec9634aab7ef0fd7f98f737ce364fba018
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-03 18:14:41 +0700

    Tinh nang dang nhap

--- api/v1/controllers/user.controller.ts
+++ api/v1/controllers/user.controller.ts
@@ -39,3 +39,39 @@ export const register = async (req: Request, res: Response) => {
     });
   }
 }
+
+export const login = async (req: Request, res: Response) => {
+    const email: string = req.body.email
+    const password: string = req.body.password;
+
+    const user = await User.findOne({
+      email: email,
+      deleted: false,
+    });
+  
+    if (!user) {
+      res.json({
+        code: 400,
+        message: "Email không tồn",
+      });
+      return;
+    }
+  
+    if (md5(password) !== user.password) {
+      res.json({
+        code: 400,
+        message: "Mật khẩu không đúng",
+      });
+      return;
+    }
+    
+    //Trả cookie về cho FE
+    const token = user.token;
+    res.cookie("token", token);
+  
+    res.json({
+      code: 200,
+      message: "Đăng nhập thành công",
+      token: token,
+    });
+}
