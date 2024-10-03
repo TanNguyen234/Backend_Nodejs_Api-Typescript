1b5358f76f4f679e4c3c402dcb45bbab7cd98ef5
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-03 15:13:46 +0700

    Bộ lọc theo trang thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,9 +2,20 @@ import { Request, Response } from 'express'
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
+    interface find {
+        deleted: boolean,
+        status?: string,
+    }
+
+    const find: find = {
         deleted: false,
-    })
+    }
+
+    if(req.query.status) {
+        find.status = req.query.status.toString()
+    }
+
+    const tasks = await Task.find(find)
 
     res.json({
         code: 200,
