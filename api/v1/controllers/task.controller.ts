3db3e3fa32234ee875542861e6b3cf7b02e52d14
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-03 15:32:31 +0700

    Sap xap theo tieu chi

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,20 +2,33 @@ import { Request, Response } from 'express'
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
-    interface find {
+    //Find
+    interface Find {
         deleted: boolean,
         status?: string,
     }
 
-    const find: find = {
+    const find: Find = {
         deleted: false,
     }
 
     if(req.query.status) {
         find.status = req.query.status.toString()
     }
+    //End Find
+    //Sort
+    interface Sort {
+        [key: string]: any;//Đánh dấu chỉ mục là chuỗi trước khi gán vào
+    }
+    const sort: Sort = {}
 
-    const tasks = await Task.find(find)
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey = req.query.sortKey.toLocaleString();
+        sort[sortKey] = req.query.sortValue;
+    }
+    //End Sort
+
+    const tasks = await Task.find(find).sort(sort)
 
     res.json({
         code: 200,
