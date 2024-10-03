c47846aebb866895df97a60601ac085634ec351c
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-03 17:33:04 +0700

    Cai dat cors

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import express, { Express } from "express"
-import dotenv from "dotenv"
-import * as database from './config/database'
-import mainV1Routes from "./api/v1/routes/index.route"
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from 'cors'
+import * as database from './config/database';
+import mainV1Routes from "./api/v1/routes/index.route";
 
 dotenv.config()
 
@@ -9,6 +10,13 @@ database.connect() // kết nối với database
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000
+
+// var corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }    
+// app.use(cors(corsOptions));
+app.use(cors()) // enable cors
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
